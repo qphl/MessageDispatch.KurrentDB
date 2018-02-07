7b111ccd8117224e1ac285b6ed8f6eae9e762153
Rich Knight <rich@rich-knight.com>
2018-02-07 21:40:04 +0000

    Remove accidental references

--- src/dispatchers.eventstore/SimpleEventStoreDispatcher.cs
+++ src/dispatchers.eventstore/SimpleEventStoreDispatcher.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using CR.MessageDispatch.Core;
 using EventStore.ClientAPI;
